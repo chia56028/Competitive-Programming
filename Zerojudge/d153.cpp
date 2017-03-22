77c79bc4ef6f7a85394c0294fb36c024e6320292
ching-yu <chia56028@gmail.com>
2017-03-22 20:00:48 +0800

    add

---
+++ Zerojudge/d153.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int m, n, tmp;
+    cin>>m;
+    for(int x=1; x<=m; x++){
+      while(cin>>n){
+        int arr[n];
+        for(int i=0; i<n; i++){
+            cin>>arr[i];
+        }
+        
+        sort(arr,arr+n);
+        
+        if(n%2==0)
+            cout<<arr[n/2-1]<<endl;
+        if(n%2!=0)
+            cout<<arr[(n+1)/2-1]<<endl;
+      }
+    }
+    return 0;
+}
