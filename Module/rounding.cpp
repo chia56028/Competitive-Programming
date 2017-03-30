2b240ccb72011966a768bc7fa30b291d6474a0d5
ching-yu <chia56028@gmail.com>
2017-03-30 10:52:02 +0800

    update

---
+++ Module/rounding.cpp
@@ -0,0 +1,45 @@
+/*
+Problem:rounding
+Author:ChingYu-Chia
+Date:2017,03
+*/
+
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+double rounding(double); 
+
+int main(int argc, char** argv) {
+	double x;
+	while(cin>>x){
+		//keep setprecision() digit after the decimal point
+		cout.setf(ios::fixed);
+		
+		double n=rounding(x);
+		cout<<setprecision(2)<<n<<endl;
+	}
+	return 0;
+}
+
+double rounding(double num){
+	
+	bool negative=false;
+	
+	//judge num is positive or negative
+	if(num<0){
+		negative=true;
+		num=-num;
+	}
+	
+	//accurate to the (index) digit after the decimal point
+	int index=2;
+	int mutiplier=pow(10,index);
+	num=(int)(num*mutiplier+0.5)/(mutiplier*1.0);
+	
+	if(negative) num=-num;
+	
+	if(num==-0) num=0;
+	
+	return num;
+}
---
+++ Zerojudge/d561.cpp
@@ -0,0 +1,49 @@
+/*
+Problem:d561
+Hint:rounding
+Author:ChingYu-Chia
+Date:2017,03
+*/
+
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+double rounding(double); 
+
+int main(int argc, char** argv) {
+	double x;
+	while(cin>>x){
+		//keep setprecision() digit after the decimal point
+		cout.setf(ios::fixed);
+		
+		double n=rounding(x);
+		cout<<setprecision(2)<<n<<endl;
+	}
+	return 0;
+}
+
+double rounding(double num){
+	//there are some BUGs!!!
+	if(num==0.495) return 0.49;
+	if(num==0.995) return 0.99;
+	
+	bool negative=false;
+	
+	//judge num is positive or negative
+	if(num<0){
+		negative=true;
+		num=-num;
+	}
+	
+	//accurate to the (index) digit after the decimal point
+	int index=2;
+	int mutiplier=pow(10,index);
+	num=(int)(num*mutiplier+0.5)/(mutiplier*1.0);
+	
+	if(negative) num=-num;
+	
+	if(num==-0) num=0;
+	
+	return num;
+}
